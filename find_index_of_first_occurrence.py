2868593d4c58eeff565047459e69c0d7726d1227
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-07-07 16:05:57 +0530

    Add files via upload

---
+++ find_index_of_first_occurrence.py
@@ -0,0 +1,36 @@
+
+def strStr(haystack, needle):
+    """
+    :type haystack: str
+    :type needle: str
+    :rtype:
+    """
+    
+    if len(needle)>len(haystack):
+        return -1
+    else:
+        for j in range(len(haystack)):
+            i=0
+            count=0
+            if needle[i]==haystack[j]:
+                n=0
+                for m in range(j,len(haystack)):  
+                        if n==len(needle):
+                            break
+                        else :
+                            if needle[n]==haystack[m]:
+                                count+=1
+                        n=n+1
+             
+                if count==len(needle):
+                    break
+        if count==len(needle):
+            return j
+        else:
+            return -1
+if __name__=="__main__":
+    
+    haystack="leetcode"
+    needle="code"
+    result=strStr(haystack, needle)
+    print(f"index of first occurrence of  {needle} in {haystack} is {result}".format(needle,haystack,result))    
