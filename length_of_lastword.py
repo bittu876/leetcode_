7e271b5314ab360048986e4b519e164154df01a4
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-07-07 16:06:26 +0530

    Add files via upload

---
+++ length_of_lastword.py
@@ -0,0 +1,20 @@
+
+def lengthOfLastWord( s):
+    """
+    :type s: str
+    :rtype: int
+    """
+    s=s.strip()
+    count=0
+    for i in range(len(s)-1,-1,-1):
+        if count!=0 and s[i]==" ":
+            break
+        else:
+            count+=1
+    return count
+if __name__=="__main__":
+    
+    s="hello world"
+    result=lengthOfLastWord(s)
+    print("length of last word is",result)
+   
