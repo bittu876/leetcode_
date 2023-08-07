0ec3e9855111d3952c1eecf2ea6e4f9356a5c7a5
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-08-08 00:30:58 +0530

    Update remove_duplicates.py

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,4 +1,5 @@
-
+#Remove Duplicates from Sorted Array
+#https://leetcode.com/problems/remove-duplicates-from-sorted-array/submissions/
 def removeDuplicates(nums):
     """
     :type nums: List[int]
