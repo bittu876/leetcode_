489ebe7e7851298357c0223e5c89b45e1d80146e
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-08-17 22:40:16 +0530

    Update length_of_lastword.py

--- length_of_lastword.py
+++ length_of_lastword.py
@@ -1,5 +1,5 @@
 #Length of Last Word
-#https://leetcode.com/problems/length-of-last-word/submissions/
+#https://leetcode.com/problems/length-of-last-word/description/
 
 def lengthOfLastWord( s):
     """
