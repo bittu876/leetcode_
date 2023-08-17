3c6bf31d2b7e69069944ca6f96b5ced55cb7c042
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-08-17 22:39:26 +0530

    Update divide.py

--- divide.py
+++ divide.py
@@ -1,5 +1,5 @@
 #29. Divide Two Integers
-#https://leetcode.com/problems/divide-two-integers/discription/
+#https://leetcode.com/problems/divide-two-integers/description/
 
 def divide(dividend, divisor):
         """
