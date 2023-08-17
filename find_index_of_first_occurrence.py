0b14712456092ce0977cd748bb932192fb5dbfd2
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-08-17 22:39:12 +0530

    Update find_index_of_first_occurrence.py

--- find_index_of_first_occurrence.py
+++ find_index_of_first_occurrence.py
@@ -1,4 +1,4 @@
-#https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/submissions/
+#https://leetcode.com/problems/find-the-index-of-the-first-occurrence-in-a-string/description/
 #Find the Index of the First Occurrence in a String
 
 
