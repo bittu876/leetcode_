3f1bb013a6fd5103ea7da862d4455364bfe9cf52
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-08-17 22:36:45 +0530

    Update removeNthFromEnd.py

--- removeNthFromEnd.py
+++ removeNthFromEnd.py
@@ -1,3 +1,6 @@
+
+#19. Remove Nth Node From End of List
+#https://leetcode.com/problems/remove-nth-node-from-end-of-list/description/
 class ListNode(object):
      def __init__(self, val=0, next=None):
         self.val = val
