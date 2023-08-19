63004934cb430a380d7898f5642321919302462c
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-08-19 22:42:46 +0530

    Add files via upload

---
+++ isSameTree.py
@@ -0,0 +1,34 @@
+class TreeNode(object):
+     def __init__(self, val=0, left=None, right=None):
+         self.val = val
+         self.left = left
+         self.right = right
+
+def isSameTree (p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+        
+        def check(rot):
+            res=[] 
+            def checkInorder(root):
+                if root == None:
+                    res.append("0")
+                    return
+                res.append(root.val)
+                checkInorder(root.left)
+                
+                checkInorder(root.right)
+            checkInorder(rot)
+            return res
+        if check(p)==check(q):
+            return True
+        else:
+            return False
+x=TreeNode(5,TreeNode(3,TreeNode(1,None,None),TreeNode(8,None,None)),TreeNode(1,TreeNode(1,None,None),TreeNode(1,None,None)))
+y=TreeNode(5,TreeNode(3,TreeNode(1,None,None),TreeNode(8,None,None)),TreeNode(1,TreeNode(1,None,None),TreeNode(1,None,None)))
+result=isSameTree(x,y)
+print(result)
+        
