feb5b62299e683b28d5c36f579012da96c9493df
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-09-07 11:51:54 +0530

    Update 0118-pascals-triangle.py

--- 0118-pascals-triangle/0118-pascals-triangle.py
+++ 0118-pascals-triangle/0118-pascals-triangle.py
@@ -4,11 +4,20 @@ class Solution(object):
         :type numRows: int
         :rtype: List[List[int]]
         """
+        # creating a structur for out put filling with 1
+        #  if numRows = 4  print(x)  ==> 
+        #            1
+        #           1  1
+        #          1  1  1   
+        #         1  1  1  1
         x= [[1 for i in range(x)] for x in range(1,numRows+1)]
         for i in  range(len(x)):
             for j in range(len(x[i])):
                 if j != 0 and j != len(x[i])-1:
                     x[i][j]=x[i-1][j-1]+x[i-1][j]
+        # this nested forloop will change the values in it
+        #if the pointr is at starting or ending of the row it doesn't change anything
+        #if it is middle of the row it change value by adding previos row precedig col value and current col value
         return x
 
         
