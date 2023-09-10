ed0ea69736da4d754e9908938b56a734fb7ce379
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-09-10 23:04:16 +0530

    Time: 16 ms (67.03%), Space: 13.4 MB (5.5%) - LeetHub

---
+++ 0075-sort-colors/0075-sort-colors.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def sortColors(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: None Do not return anything, modify nums in-place instead.
+        """
+        
+        return nums.sort()
+
+
+            
+        
+        
