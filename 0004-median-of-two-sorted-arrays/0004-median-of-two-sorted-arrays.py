f5a7a8a9897f0d25961e56d237980b151a88961d
bittu876 <121486439+bittu876@users.noreply.github.com>
2023-09-21 21:31:46 +0530

    Time: 75 ms (20.55%), Space: 13.3 MB (88.11%) - LeetHub

--- 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
+++ 0004-median-of-two-sorted-arrays/0004-median-of-two-sorted-arrays.py
@@ -8,7 +8,6 @@ class Solution:
         """
         nums3=nums1+nums2
         nums3.sort()
-        print(1.0/2.0)
         if  len(nums3)%2==0:
             return ((float(nums3[len(nums3)//2])+float(nums3[(len(nums3)/2)-1]))/2)
         else:
